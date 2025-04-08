a1ccfaa8e3b03e013899f1dd6cf648f1e11acea4
universea <287827688@qq.com>
2025-04-08 12:46:56 +0800

    auto_coder_pre_000000000013_chat_action.yml_059eff8e6454cf1b496779b79bc3e2f8

--- gscientist/core/projects.py
+++ gscientist/core/projects.py
@@ -2,18 +2,18 @@ from gscientist.db.session import get_db
 from gscientist.db.models import Project
 
 class ProjectService:
-    """科研项目管理服务"""
+    """锟斤拷锟斤拷锟斤拷目锟斤拷锟斤拷锟斤拷锟斤拷"""
     
     def __init__(self):
         self.db = get_db()
 
     def create_project(self, name: str):
-        """创建新项目"""
+        """锟斤拷锟斤拷锟斤拷锟斤拷目"""
         project = Project(name=name)
         self.db.add(project)
         self.db.commit()
         return project
 
     def list_projects(self):
-        """列出所有项目"""
+        """锟叫筹拷锟斤拷锟斤拷锟斤拷目"""
         return self.db.query(Project).all()
